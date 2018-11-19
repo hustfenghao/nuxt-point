fd2b37be002dc403c22c5046c8876585e7993ae4
hustfenghao <37919589+hustfenghao@users.noreply.github.com>
2018-11-19 09:44:12 +0900

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,14 +1,4 @@
 import firebase from 'firebase'
-  // Initialize Firebase
-  if (!firebase.apps.length) {
-    firebase.initializeApp({
-      apiKey: "AIzaSyDd6wbr4oJokHZmFufm-ucXXyO-Qefn1Fk",
-      authDomain: "apppoint-1d4ec.firebaseapp.com",
-      databaseURL: "https://apppoint-1d4ec.firebaseio.com",
-      projectId: "apppoint-1d4ec",
-      storageBucket: "apppoint-1d4ec.appspot.com",
-      messagingSenderId: "267332444180"
-    })
-  }
+  
 
   export default firebase
